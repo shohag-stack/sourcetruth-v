ea96dd23e276d8ecedb529fefc3b69e6f79885b4
shohag-stack <shohag4y@gmail.com>
2026-07-14 02:30:28 +0600

    fixed background color code

--- components/layout/AppShell.tsx
+++ components/layout/AppShell.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from './Sidebar'
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-[#F8F9FC]">
+    <div className="flex min-h-screen">
       <Sidebar />
       <main className="flex-1 ml-[220px] min-h-screen">
         {children}
