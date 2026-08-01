e27ce957f3753e4e993f7715c3d166c8acf90a58
shohag-stack <shohag4y@gmail.com>
2026-08-02 01:47:08 +0600

    fixed dummy data import features in analytics page

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,7 +1,6 @@
 // app/analytics/page.tsx
 import { AppShell } from "@/components/layout/AppShell";
 import { RevenueAreaChart } from "@/components/charts/RevenueAreaChart";
-import { PLATFORM_META } from "@/lib/dummy-data.tsX"; // static branding lookup only
 import { formatMoneyFull, formatNumber, trendLabel } from "@/lib/utils";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
